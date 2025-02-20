894d4cfb28e4ade3856752a3d119edc17cf49bfb
AsiaEasa <138696944+AsiaEasa@users.noreply.github.com>
2025-02-20 19:49:57 +0200

    Update iot-device.h

--- src/network/model/iot-device.h
+++ src/network/model/iot-device.h
@@ -7,20 +7,26 @@ namespace ns3 {
 
 class IoTDevice {
 public:
-    IoTDevice(double cpu, double energy, double bandwidth, bool charge);
+    IoTDevice(double cpu, double energy, double bandwidth, bool charge, double chargingRate);
     double GetCpuFrequency() const;
     double GetEnergy() const;
     double GetBandwidth() const;
     bool HasWirelessCharging() const;
+    void UpdateEnergy(double energyConsumed, int chargingEnergy);
 
 private:
     double m_cpuFrequency;
     double m_energy;
     double m_bandwidth;
     bool m_wirelessCharging;
+    double m_initialEnergy;
+    double m_chargingRate;
 };
 
 std::vector<IoTDevice> GenerateIoTDevices(int numDevices);
+double ComputeEnergyConsumption(double cpuFreq, double tau, double mu, double G);
+int GenerateChargingEnergy(double avgRate);
+
 
 } // namespace ns3
 
