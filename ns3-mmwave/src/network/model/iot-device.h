61672dc4f2bdbabfbc19a0d2bac9f977d280d47e
AsiaEasa <138696944+AsiaEasa@users.noreply.github.com>
2025-02-22 21:19:07 +0200

    Create iot-device.h

---
+++ ns3-mmwave/src/network/model/iot-device.h
@@ -0,0 +1,38 @@
+#ifndef IOT_DEVICE_H
+#define IOT_DEVICE_H
+
+#include "ns3/core-module.h"
+#include "ns3/mmwave-helper.h"
+#include "ns3/node.h"
+#include "ns3/network-module.h"
+
+namespace ns3 {
+
+class IoTDevice {
+public:
+    IoTDevice(double cpu, double energy, double bandwidth, bool charge);
+    double GetCpuFrequency() const;
+    double GetEnergy() const;
+    double GetBandwidth() const;
+    bool HasWirelessCharging() const;
+    void UpdateEnergy(double energyConsumed, int chargingEnergy);
+    void ConnectTo5GNetwork(ns3::Ptr<ns3::mmwave::MmWaveHelper> mmwaveHelper,
+        ns3::Ptr<ns3::Node> ueNode, 
+        ns3::Ptr<ns3::Node> gnbNode);
+
+private:
+    double m_cpuFrequency;
+    double m_energy;
+    double m_bandwidth;
+    bool m_wirelessCharging;
+    double m_initialEnergy;
+};
+
+std::vector<IoTDevice> GenerateIoTDevices(int numDevices);
+double ComputeEnergyConsumption(double cpuFreq, double tau, double mu, double G);
+int GenerateChargingEnergy(double avgRate);
+
+
+} // namespace ns3
+
+#endif // IOT_DEVICE_H
